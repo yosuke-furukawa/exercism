2b90b46e4cb223b727f498d9fa090f5ea8de9e71
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-09 19:17:41 +0900

    refactor raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn raindrops(n: u32) -> String {
-    match (n % 3, n % 5, n % 7) {
-        (0, 0, 0) => "PlingPlangPlong".to_string(),
-        (0, 0, _) => "PlingPlang".to_string(),
-        (0, _, 0) => "PlingPlong".to_string(),
-        (_, 0, 0) => "PlangPlong".to_string(),
-        (0, _, _) => "Pling".to_string(),
-        (_, 0, _) => "Plang".to_string(),
-        (_, _, 0) => "Plong".to_string(),
-        (_, _, _) => n.to_string(),
+    let factors = [(3, "Pling"), (5, "Plang"), (7, "Plong")];
+    let output = factors
+        .iter()
+        .map(|factor| if n % factor.0 == 0 { factor.1 } else { "" })
+        .collect::<String>();
+
+    match output.is_empty() {
+        true => n.to_string(),
+        false => output,
     }
 }
