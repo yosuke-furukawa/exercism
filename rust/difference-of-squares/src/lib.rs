8e07ecbed6d02d0fe06c7e93fb3cd4c20996f2fc
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-11 22:59:23 +0900

    Add difference of squares

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -1,6 +1,6 @@
 pub fn square_of_sum(n: u32) -> u32 {
     let mut sum = 0;
-    for i in 1..n {
+    for i in 1..=n {
         sum = sum + i;
     }
     sum*sum
@@ -8,12 +8,12 @@ pub fn square_of_sum(n: u32) -> u32 {
 
 pub fn sum_of_squares(n: u32) -> u32 {
     let mut sum = 0;
-    for i in 1..n {
+    for i in 1..=n {
         sum = sum + i * i;
     }
     sum
 }
 
 pub fn difference(n: u32) -> u32 {
-    sum_of_squares(n) - square_of_sum(n)
+    square_of_sum(n) - sum_of_squares(n)
 }
