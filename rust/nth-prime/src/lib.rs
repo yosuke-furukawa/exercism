4a3987a1b0f3b9b4cb4d3db1ea3b486c3161d4be
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-13 14:55:48 +0900

    refactor nth prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,23 +1,13 @@
 pub fn nth(n: u32) -> u32 {
-    let mut primes = vec![2, 3, 5, 7, 11, 13];
-    let mut current = 15;
-    let mut length = primes.len() as u32;
-    while length <= n {
-        let mut found = false;
-        for v in &primes {
-            if current % v == 0 {
-                found = true;
-                break;
-            }
-        }
+    let mut p = 3;
+    let mut primes = vec![2];
 
-        if found {
-            current = current + 2;
-            continue;
+    while n >= primes.len() as u32 {
+        if !primes.iter().any(|pn| p % pn == 0) {
+            primes.push(p);
         }
-        primes.push(current);
-        length = length + 1;
+        p += 1;
     }
-    
-    return *primes.get(n as usize).unwrap_or(&0);
+
+    primes.pop().unwrap()
 }
