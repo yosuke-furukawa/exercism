2b627791ad3c42c7f1e0c1b2601b8c8b0b699fe3
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-30 15:46:31 +0900

    refactor series

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,14 +1,10 @@
+use std::iter::FromIterator;
+
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut result = vec![];
-    if len == 0 {
-        for _ in digits.as_bytes() {
-            result.push("".to_string());
-        }
-        result.push("".to_string());
-        return result;
-    }
-    for s in digits.as_bytes().windows(len) {
-        result.push(std::str::from_utf8(&s).unwrap_or("").to_string());
-    }
-    result
+    digits
+        .chars()
+        .collect::<Vec<char>>()
+        .windows(len)
+        .map(|f| String::from_iter(f.iter()))
+        .collect::<Vec<String>>()
 }
