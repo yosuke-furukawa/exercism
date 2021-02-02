6ce7d2d4985cf79a9102e325e0f25c1080d614e1
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-02-02 10:09:57 +0900

    Refactor etl

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,14 +1,12 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut map = BTreeMap::new();
-    for v in h.keys() {
-        let keys = h.get(v).unwrap();
-        for k in keys {
-            let mut newk = *k;
-            newk.make_ascii_lowercase();
-            map.insert(newk, *v);
-        }
-    }
-    map
+    h.into_iter()
+        .map(|(k, v)| {
+            v.iter()
+                .map(|c| (c.to_ascii_lowercase(), *k))
+                .collect::<Vec<(char, i32)>>()
+        })
+        .flatten()
+        .collect()
 }
