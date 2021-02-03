acc7e3a6d51202fd8e5e259118bfb5e8c7310bda
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-02-03 12:39:22 +0900

    refactor sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,13 +1,9 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let mut nums = (2..=upper_bound).collect::<Vec<u64>>();
-    let mut prime = 2;
-    let mut primes = vec![prime];
-    while prime < upper_bound {
-        nums = nums.iter().filter(|n| { *n % prime != 0 }).cloned().collect();
-        if nums.len() == 0 {
-            break;
-        }
-        prime = nums[0];
+    let mut primes = vec![];
+    while !nums.is_empty() {
+        let prime = nums[0];
+        nums = nums.iter().filter(|n| *n % prime != 0).cloned().collect();
         primes.push(prime);
     }
     primes
