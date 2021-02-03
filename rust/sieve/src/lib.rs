5c9a82ce83c19516bf0080d7c14981115187a009
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-02-03 12:43:21 +0900

    use retain method instead of filter reassign

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut nums = (2..=upper_bound).collect::<Vec<u64>>();
+    let mut nums: Vec<u64> = (2..=upper_bound).collect();
     let mut primes = vec![];
     while !nums.is_empty() {
         let prime = nums[0];
-        nums = nums.iter().filter(|n| *n % prime != 0).cloned().collect();
+        nums.retain(|&n| n % prime != 0);
         primes.push(prime);
     }
     primes
