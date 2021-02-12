3ac363969148cab9c7cf16fca8ce8bd8353c4072
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-02-12 23:29:34 +0900

    refactor bowling

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -4,6 +4,7 @@ pub enum Error {
     GameComplete,
 }
 
+#[derive(Default)]
 pub struct BowlingGame {
     frames: [Option<u16>; 21],
     rounds: usize,
@@ -12,7 +13,7 @@ pub struct BowlingGame {
 
 impl BowlingGame {
     pub fn new() -> Self {
-        BowlingGame{ frames: [None; 21], rounds: 0, finished: false }
+        BowlingGame::default()
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
@@ -27,37 +28,34 @@ impl BowlingGame {
             if self.rounds % 2 == 1 && self.frames[self.rounds - 1].unwrap() + pins > 10 {
                 return Err(Error::NotEnoughPinsLeft);
             }
-            
+
             self.frames[self.rounds] = Some(pins);
             if pins == 10 && self.rounds % 2 == 0 {
                 self.rounds += 2;
             } else {
                 self.rounds += 1;
             }
-        } else {
-            if self.rounds == 18 {
-                self.frames[self.rounds] = Some(pins);
-                self.rounds += 1;
-            } else if self.rounds == 19 {
-                let prev_pin = self.frames[18].unwrap();
-                if prev_pin < 10 && prev_pin + pins > 10 {
-                    return Err(Error::NotEnoughPinsLeft);
-                }
-                if prev_pin + pins < 10 {
-                    self.finished = true;
-                }
-                self.frames[self.rounds] = Some(pins);
-                self.rounds += 1;
-            } else {
-                let pin18 = self.frames[18].unwrap();
-                let pin19 = self.frames[19].unwrap();
-                if pin18 == 10 && pin19 < 10 && pin19 + pins > 10 {
-                    return Err(Error::NotEnoughPinsLeft);
-                }
-                self.frames[self.rounds] = Some(pins);
+        } else if self.rounds == 18 {
+            self.frames[self.rounds] = Some(pins);
+            self.rounds += 1;
+        } else if self.rounds == 19 {
+            let prev_pin = self.frames[18].unwrap();
+            if prev_pin < 10 && prev_pin + pins > 10 {
+                return Err(Error::NotEnoughPinsLeft);
+            }
+            if prev_pin + pins < 10 {
                 self.finished = true;
             }
-            
+            self.frames[self.rounds] = Some(pins);
+            self.rounds += 1;
+        } else {
+            let pin18 = self.frames[18].unwrap();
+            let pin19 = self.frames[19].unwrap();
+            if pin18 == 10 && pin19 < 10 && pin19 + pins > 10 {
+                return Err(Error::NotEnoughPinsLeft);
+            }
+            self.frames[self.rounds] = Some(pins);
+            self.finished = true;
         }
 
         Ok(())
@@ -70,23 +68,29 @@ impl BowlingGame {
         let mut score = 0;
         for i in 0..9 {
             let index1 = i * 2;
-            let index2 = (i+1) * 2;
-            let index3 = (i+2) * 2;
-            score += match (self.frames[index1], self.frames[index1+1], self.frames[index2], self.frames[index2 + 1], self.frames[index3]) {
+            let index2 = (i + 1) * 2;
+            let index3 = (i + 2) * 2;
+            score += match (
+                self.frames[index1],
+                self.frames[index1 + 1],
+                self.frames[index2],
+                self.frames[index2 + 1],
+                self.frames[index3],
+            ) {
                 (Some(a), Some(b), Some(c), _, _) if a + b == 10 => a + b + c, // Spare
-                (Some(a), _, Some(c), Some(d), _) if a == 10 => a + c + d, // Strike
-                (Some(a), _, Some(c), None, Some(e)) if a == 10 => a + c + e, // Strike
+                (Some(a), _, Some(c), Some(d), _) if a == 10 => a + c + d,     // Strike
+                (Some(a), _, Some(c), None, Some(e)) if a == 10 => a + c + e,  // Strike
                 (Some(a), Some(b), _, _, _) => a + b,
                 _ => 0,
             };
         }
-        score += match(self.frames[18], self.frames[19], self.frames[20]) {
+        score += match (self.frames[18], self.frames[19], self.frames[20]) {
             (Some(a), Some(b), Some(c)) => a + b + c,
             (Some(a), Some(b), _) => a + b,
             (Some(a), _, _) => a,
-            _ => 0
+            _ => 0,
         };
-        
+
         Some(score)
     }
 }
