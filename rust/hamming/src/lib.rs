0d5581dbd5552c938c784cff185640921c4b22f3
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-03-23 00:47:52 +0900

    refactor hamming

--- rust/hamming/src/lib.rs
+++ rust/hamming/src/lib.rs
@@ -1,17 +1,11 @@
 /// Return the Hamming distance between the strings,
 /// or None if the lengths are mismatched.
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    let c1: Vec<char> = s1.chars().collect();
-    let c2: Vec<char> = s2.chars().collect();
-    let mut diff = (s1.len() as i32 - s2.len() as i32).abs();
-    if diff != 0 {
+    if s1.len() != s2.len() {
         return None;
     }
-    let len = if s1.len() > s2.len() { s2.len() } else { s1.len() };
-    for i in 0..len {
-        if c1[i] != c2[i] {
-            diff += 1;
-        }
-    }
-    Some(diff as usize)
+
+    Some(
+        s1.chars().zip(s2.chars()).filter(|(c1, c2)| c1 != c2).count()
+    )
 }
